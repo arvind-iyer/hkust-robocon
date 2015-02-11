129a763622eafdc186c6ed84207565dafb463f90
hautonjt <hautonjt@gmail.com>
2015-02-11 13:35:25 +0800

    last commit with encoder for calibration before deleting

--- stm32f103VF_template/src/user/racket_control.h
+++ stm32f103VF_template/src/user/racket_control.h
@@ -7,7 +7,7 @@
 #include "special_char_handler.h"
 #include "stdbool.h"
 
-//#define	RACKET_TIMEOUT					25
+#define	SWITCH_TIMEOUT					10
 #define	PNEUMATIC_OPEN_DELAY	  10
 #define MOTOR_OPEN_DELAY        10
 #define ENCODER_THRESHOLD				10000
