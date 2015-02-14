d139376fbdc12b3536af99a779406e97163beea9
hjchoi95 <hongjoonchoi95@gmail.com>
2015-02-14 22:58:28 +0800

    changed a bit of acc value

--- Robot_C/Robot_C_auto_positioning/src/user/wheel_base.h
+++ Robot_C/Robot_C_auto_positioning/src/user/wheel_base.h
@@ -36,10 +36,10 @@ static const u16 SPEED_MODES[10] =	// In percentage (20 = 20%)
 #define	WHEEL_BASE_DEFAULT_SPEED_MODE					3			// from 0 to 9
 
 // Wheel base motors acceleration (CONSTANT, to be configured upon startup)
-#define	WHEEL_BASE_BR_ACC											200		// Bottom-right wheel
-#define	WHEEL_BASE_BL_ACC										  200		// Bottom-left wheel
-#define	WHEEL_BASE_TL_ACC											200		// Top-left wheel
-#define	WHEEL_BASE_TR_ACC											200 		// Top-right wheel
+#define	WHEEL_BASE_BR_ACC											600		// Bottom-right wheel
+#define	WHEEL_BASE_BL_ACC										  600		// Bottom-left wheel
+#define	WHEEL_BASE_TL_ACC											600		// Top-left wheel
+#define	WHEEL_BASE_TR_ACC											600 		// Top-right wheel
 
 typedef struct {
 	s32 x;
