7d9273aa8ce3faac10d1cb22373b076479b2b617
Arvind Iyer <arvindi.me@gmail.com>
2015-04-22 19:13:52 +0800

    Compatibility fix

--- Robot_C/src/user/wheel_base.h
+++ Robot_C/src/user/wheel_base.h
@@ -38,10 +38,10 @@ static const u16 SPEED_MODES[10] =	// In percentage (20 = 20%)
 #define	WHEEL_BASE_DEFAULT_SPEED_MODE					3			// from 0 to 9
 
 // Wheel base motors acceleration (CONSTANT, to be configured upon startup)
-#define	WHEEL_BASE_BR_ACC											250		// Bottom-right wheel
-#define	WHEEL_BASE_BL_ACC										  250		// Bottom-left wheel
-#define	WHEEL_BASE_TL_ACC											90		// Top-left wheel
-#define	WHEEL_BASE_TR_ACC											90 		// Top-right wheel
+#define	WHEEL_BASE_BR_ACC											ROBOT=='C'?300:250		// Bottom-right wheel
+#define	WHEEL_BASE_BL_ACC										  ROBOT=='C'?300:250// Bottom-left wheel
+#define	WHEEL_BASE_TL_ACC											ROBOT=='C'?300:90 // Top-left wheel
+#define	WHEEL_BASE_TR_ACC											ROBOT=='C'?300:90 		// Top-right wheel
 
 #define XY_BR_SCALE 100
 #define XY_BL_SCALE 100
